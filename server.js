eafa6b20ac4df434d15252e4ae7cb9034cf860df
Andrew Fry <andyfry01@gmail.com>
2016-05-17 09:36:59 -0400

    testing static server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var PORT = 31415;
+var app = express();
+
+app.use(express.static('./public'))
+
+app.listen(PORT, function(){
+  console.log("server running on port", PORT);
+})
