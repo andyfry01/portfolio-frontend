6b3d5e718e1e0ecaebf1259cae076c075c1fc6c1
Andy Fry <andyfry01@gmail.com>
2017-01-14 16:34:20 -0500

    more experimenting with text bg

--- testFiles/app.js
+++ testFiles/app.js
@@ -2,11 +2,33 @@ const skillsText = "skills"
 const frontendText = "frontend"
 const backendText = "backend"
 
+
+
+/** Fills target divs with specified content
+  @param target {string} target container ID
+  @param content {string} the words/content string for the fill content
+  @param contentTagType {string} type of tag for fill content (i.e. span, div, p)
+  @param fillNum {number} number of iterations for the loop that generates the word spans
+*/
+
 const fillTargetWithContent = function(target, content, contentTagType, fillNum){
+
+  const colors = [
+    {orange: 'rgba(255, 107, 53, 0.45)'},
+    {tan: 'rgba(251, 242, 192, 0.45)'},
+    {blue: 'rgba(63, 124, 172, 0.45)'}
+  ]
 
   // Generates random font size
   const randomFontSize = function(max, min){
     return Math.random() * (max - min + 1) * 0.5 + min + 'em'
+  }
+
+  // Picks random color from color array
+  const randomColor = function(colors){
+    colorsLen = colors.length
+    let index = Math.floor(Math.random() * colorsLen)
+    return Object.values(colors[index])[0]
   }
 
   // fills target divs with content text
@@ -15,11 +37,13 @@ const fillTargetWithContent = function(target, content, contentTagType, fillNum)
     let fillWord = document.createElement(contentTagType)
     fillWord.innerHTML = content + " "
     fillWord.style.fontSize = randomFontSize(10, 1)
+    fillWord.style.color = randomColor(colors)
     targetDiv.appendChild(fillWord)
   }
 }
 
 window.onload = function(){
-  fillTargetWithContent('target', skillsText, 'span', 200)
-  fillTargetWithContent('targetTwo', frontendText, 'span', 200)
+  fillTargetWithContent('targetOne', skillsText, 'span', 400)
+  fillTargetWithContent('targetTwo', frontendText, 'span', 400)
+  fillTargetWithContent('targetThree', backendText, 'span', 400)
 }
